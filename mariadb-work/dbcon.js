3d2b7839f8d102ddf32d52125c90e9b5efa92f2e
Minuk1008 <kimmw1008@gmail.com>
2021-11-26 19:58:04 +0900

    11.26 / class mysql

--- mariadb-work/dbcon.js
+++ mariadb-work/dbcon.js
@@ -1,14 +1,4 @@
 const mysql = require("mysql");
 const config = require("./dbconfig.json");
 
-// let dbPool = mysql.createPool(congif); //const 사용하면 안됌
-
-// function getDbConnection(callback) {
-//     dbPool.getConnection((err, conn) => {
-//         if(!err) {
-//             callback(conn);
-//         }
-//     })
-// };
-
 module.exports=mysql.createPool(config);
--- mariadb-work/index.js
+++ mariadb-work/index.js
@@ -6,8 +6,49 @@ const path = require("path");
 // const getDbConnection = require('./dbcon');
 const app = express();
 
-mysql.getDbConnection((err, connection) => {
-    if(!err) {
-        connection.query("Select ")
-    };
+const host = '127.0.0.1';
+const port = 3000;
+
+// mysql.getDbConnection((err, connection) => {
+//     if(!err) {
+//         console.log("DB Connection Pool Success");
+//     };
+//     connection.release();
+// });
+
+app.get("/connection_pool/test", (req,res) => {
+    const sql = "SELECT * FROM class4"
+    try {
+        mysql.getConnection((err, connection) => {
+            console.log("connection_pool GET");
+            if(err) throw err;
+
+            connection.query(sql, (err, result, fields) => {
+                if(err) {
+                    console.error("connection_pool GET Error / " + err);
+                    res.status(500).send("message : Internal Server Error");
+                } else {
+                    if(result.length === 0) {
+                        res.status(400).send({
+                            success : false,
+                            message : "DB response Not Found"
+                        });
+                    } else {
+                        res.status(200).send({
+                            success : true,
+                            result
+                            });
+                        }
+                    }
+            });
+            connection.release(); //반납하다
+        });
+    } catch(err) {
+        console.error("connection_pool GET Error / " + err);
+        res.status(500).send("message : Internal Server Error");
+    }
+});
+
+app.listen(port, host, () => {
+    console.log(`http://${host}:${port}/`);
 });
